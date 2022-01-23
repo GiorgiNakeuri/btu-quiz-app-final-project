9a789db3c08a4a57d4927c5766b84eed6c9c5baf
GiorgiNakeuri <nakeurigiorgi@gmail.com>
2022-01-22 19:26:16 -0800

    feat: add data fetcher, caching

--- src/pages/quiz.js
+++ src/pages/quiz.js
@@ -1,3 +1,15 @@
+import { useEffect, useState } from "react";
+import { getQuizData } from "../api/get-quiz-data";
+
 export default function Quiz() {
+  const [quizData, setQuizData] = useState();
+
+  useEffect(() => {
+    (async () => {
+      getQuizData().then((data) => setQuizData(data));
+    })();
+  }, []);
+
+  console.log(quizData);
   return <p>Quiz</p>;
 }
